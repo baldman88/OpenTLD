0ad3de5707f7d65a6f04db04209b92e02cd4c1bf
Dmitry Dolinin <dmitry.dolinin@gmail.com>
2016-08-20 11:56:58 +0300

    Changed Concurrent.hpp Changed algorithm. Also deque replaced to vector.

--- opentld/Concurrent.hpp
+++ opentld/Concurrent.hpp
@@ -11,39 +11,48 @@
 
 namespace concurrent
 {
+
 template<class InputIterator, class MapFunctor>
 InputIterator blockingMap(InputIterator first, InputIterator last, MapFunctor mapFunctor)
 {
     auto result = first;
-    auto availableThreads = std::thread::hardware_concurrency() / 2;
-    if (availableThreads < 2) {
-        while (result != last) {
+    int availableThreads = std::thread::hardware_concurrency() - 2;
+    if (availableThreads < 2)
+    {
+        while (result != last)
+        {
             mapFunctor(std::ref(*result));
             ++result;
         }
-    } else {
-        std::deque<std::future<void>> futures;
-        while (result != last) {
-            for (auto thread = futures.size(); ((thread < availableThreads) && (result != last)); ++thread) {
-                futures.push_back(std::move(std::async(std::launch::async, mapFunctor, std::ref(*result))));
-                ++result;
-            }
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if (futureIterator->wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+    }
+    else
+    {
+        std::vector<std::future<void>> futures;
+        for (int thread = 0; ((thread < availableThreads) && (result != last)); ++thread)
+        {
+            futures.push_back(std::move(std::async(std::launch::async, mapFunctor, std::ref(*result))));
+            ++result;
+        }
+        while (result != last)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if (futureIterator->wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
                     futureIterator->get();
-                    futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
+                    *futureIterator = std::move(std::async(std::launch::async, mapFunctor, std::ref(*result)));
+                    ++result;
                 }
             }
         }
-        while (futures.size() > 0) {
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if (futureIterator->wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+        while (futures.size() > 0)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if (futureIterator->wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
                     futureIterator->get();
                     futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
                 }
             }
         }
@@ -55,40 +64,47 @@ InputIterator blockingMap(InputIterator first, InputIterator last, MapFunctor ma
 template<class InputIterator, class OutputIterator, class MapFunctor>
 OutputIterator blockingMapped(InputIterator first, InputIterator last, OutputIterator result, MapFunctor mapFunctor)
 {
-    auto availableThreads = std::thread::hardware_concurrency() / 2;
-    if (availableThreads < 2) {
-        while (first != last) {
+    int availableThreads = std::thread::hardware_concurrency() - 2;
+    if (availableThreads < 2)
+    {
+        while (first != last)
+        {
             *result = mapFunctor(*first);
             ++result;
             ++first;
         }
-    } else {
+    }
+    else
+    {
         using InputType = typename std::iterator_traits<InputIterator>::value_type;
         using OutputType = typename std::result_of<MapFunctor(InputType)>::type;
-        std::deque<std::future<OutputType>> futures;
-        while (first != last) {
-            for (auto thread = futures.size(); ((thread < availableThreads) && (first != last)); ++thread) {
-                futures.push_back(std::move(std::async(std::launch::async, mapFunctor, *first)));
-                ++first;
-            }
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+        std::vector<std::future<OutputType>> futures;
+        for (int thread = 0; ((thread < availableThreads) && (first != last)); ++thread)
+        {
+            futures.push_back(std::move(std::async(std::launch::async, mapFunctor, *first)));
+            ++first;
+        }
+        while (first != last)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
                     *result = (*futureIterator).get();
                     ++result;
-                    futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
+                    *futureIterator = std::move(std::async(std::launch::async, mapFunctor, *first));
                 }
             }
         }
-        while (futures.size() > 0) {
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+        while (futures.size() > 0)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
                     *result = (*futureIterator).get();
                     ++result;
                     futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
                 }
             }
         }
@@ -101,44 +117,54 @@ template<class InputIterator, class FilterFunctor>
 InputIterator blockingFilter(InputIterator first, InputIterator last, FilterFunctor filterFunctor)
 {
     auto result = first;
-    auto availableThreads = std::thread::hardware_concurrency() / 2;
-    if (availableThreads < 2) {
-        while (first != last) {
-            if (filterFunctor(*first) == true) {
+    int availableThreads = std::thread::hardware_concurrency() - 2;
+    if (availableThreads < 2)
+    {
+        while (first != last)
+        {
+            if (filterFunctor(*first) == true)
+            {
                 *result = *first;
                 ++result;
             }
             ++first;
         }
-    } else {
-        std::deque<std::pair<InputIterator, std::future<bool>>> futures;
-        while (first != last) {
-            for (auto thread = futures.size(); ((thread < availableThreads) && (first != last)); ++thread) {
-                futures.push_back(std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first))));
-                ++first;
-            }
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-                    if ((*futureIterator).second.get() == true) {
+    }
+    else
+    {
+        std::vector<std::pair<InputIterator, std::future<bool>>> futures;
+        for (int thread = 0; ((thread < availableThreads) && (first != last)); ++thread)
+        {
+            futures.push_back(std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first))));
+            ++first;
+        }
+        while (first != last)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
+                    if ((*futureIterator).second.get() == true)
+                    {
                         *result = *((*futureIterator).first);
                         ++result;
                     }
-                    futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
+                    *futureIterator = std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first)));
                 }
             }
         }
-        while (futures.size() > 0) {
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-                    if ((*futureIterator).second.get() == true) {
+        while (futures.size() > 0)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
+                    if ((*futureIterator).second.get() == true)
+                    {
                         *result = *((*futureIterator).first);
                         ++result;
                     }
                     futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
                 }
             }
         }
@@ -150,51 +176,62 @@ InputIterator blockingFilter(InputIterator first, InputIterator last, FilterFunc
 template<class InputIterator, class OutputIterator, class FilterFunctor>
 OutputIterator blockingFiltered(InputIterator first, InputIterator last, OutputIterator result, FilterFunctor filterFunctor)
 {
-    auto availableThreads = std::thread::hardware_concurrency() / 2;
-    if (availableThreads < 2) {
-        while (first != last) {
-            if (filterFunctor(*first) == true) {
+    auto availableThreads = std::thread::hardware_concurrency() - 2;
+    if (availableThreads < 2)
+    {
+        while (first != last)
+        {
+            if (filterFunctor(*first) == true)
+            {
                 *result = *first;
                 ++result;
             }
             ++first;
         }
-    } else {
-        std::deque<std::pair<InputIterator, std::future<bool>>> futures;
-        while (first != last) {
-            for (auto thread = futures.size(); ((thread < availableThreads) && (first != last)); ++thread) {
-                futures.push_back(std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first))));
-                ++first;
-            }
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-                    if ((*futureIterator).second.get() == true) {
+    }
+    else
+    {
+        std::vector<std::pair<InputIterator, std::future<bool>>> futures;
+        for (int thread = 0; ((thread < availableThreads) && (first != last)); ++thread)
+        {
+            futures.push_back(std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first))));
+            ++first;
+        }
+        while (first != last)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
+                    if ((*futureIterator).second.get() == true)
+                    {
                         *result = *((*futureIterator).first);
                         ++result;
                     }
-                    futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
+                    *futureIterator = std::pair<InputIterator, std::future<bool>>(first, std::move(std::async(std::launch::async, filterFunctor, *first)));
                 }
             }
         }
-        while (futures.size() > 0) {
-            for (auto futureIterator = futures.begin(); futureIterator != futures.end();) {
-                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-                    if ((*futureIterator).second.get() == true) {
+        while (futures.size() > 0)
+        {
+            for (auto futureIterator = futures.begin(); futureIterator != futures.end(); ++futureIterator)
+            {
+                if ((*futureIterator).second.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+                {
+                    if ((*futureIterator).second.get() == true)
+                    {
                         *result = *((*futureIterator).first);
                         ++result;
                     }
                     futureIterator = futures.erase(futureIterator);
-                } else {
-                    ++futureIterator;
                 }
             }
         }
     }
     return result;
 }
-}
+
+} /* namespace concurrent */
 
 
 #endif /* CONCURRENT_HPP */
