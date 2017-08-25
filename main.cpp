588e451e71debe86f87ba09c4abe5518a847f011
Dmytro Dolinin <dmitry.dolinin@gmail.com>
2017-08-25 11:47:47 +0300

    Now the name of the video file is specified by command line parameter.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
@@ -65,61 +66,71 @@ void drawTargetRect(cv::Mat& frame, const cv::Rect& rect, const cv::Scalar& colo
 
 int main(int argc, char* argv[])
 {
-    cv::VideoCapture capture;
-    if (capture.open("/home/baldman/YandexDisk/samples/videos/helicopters/7.mp4") == false)
-    {
-        capture.open(0);
-    }
-    cv::namedWindow("Output");
-    cv::setMouseCallback("Output", mouseHandler);
-    char key = 0;
-    cv::Mat frame;
-    while (key != 'q')
-    {
-        if (key == 32)
-        {
-            cv::Mat tmp;
-            while (isTargetSelected == false)
-            {
-                tmp = frame.clone();
-                char k = cv::waitKey(1);
-                cv::rectangle(tmp, roi, cv::Scalar(0, 255, 0));
-                cv::imshow("Output", tmp);
-                if (k == 32)
-                {
-                    break;
-                }
-            }
-        }
-        else
-        {
-            if (capture.read(frame) == true)
-            {
-                if (isTargetSelected == true)
-                {
+	cv::VideoCapture capture;
+	bool isCaptureOpened = false;
+	if ((argc == 2) && std::ifstream(argv[1]))
+	{
+		isCaptureOpened = capture.open(argv[1]);
+	}
+	if (isCaptureOpened == false)
+	{
+		isCaptureOpened = capture.open(0);
+	}
+	if (isCaptureOpened == true)
+	{
+		cv::namedWindow("Output");
+		cv::setMouseCallback("Output", mouseHandler);
+		char key = 0;
+		cv::Mat frame;
+		while (key != 'q')
+		{
+			if (key == 32)
+			{
+				cv::Mat tmp;
+				while (isTargetSelected == false)
+				{
+					tmp = frame.clone();
+					char k = cv::waitKey(1);
+					cv::rectangle(tmp, roi, cv::Scalar(0, 255, 0));
+					cv::imshow("Output", tmp);
+					if (k == 32)
+					{
+						break;
+					}
+				}
+			}
+			else
+			{
+				if (capture.read(frame) == true)
+				{
+					if (isTargetSelected == true)
+					{
 #ifdef USE_DEBUG_INFO
-                    std::cout << "=======================================" << std::endl;
-                    auto begin = std::chrono::high_resolution_clock::now();
+						std::cout << "=======================================" << std::endl;
+						auto begin = std::chrono::high_resolution_clock::now();
 #endif // USE_LOGGING
 
-                    roi = tracker.getTargetRect(frame, roi);
+						roi = tracker.getTargetRect(frame, roi);
 
 #ifdef USE_DEBUG_INFO
-                    auto end = std::chrono::high_resolution_clock::now();
-                    std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n" << std::endl;
+						auto end = std::chrono::high_resolution_clock::now();
+						std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n" << std::endl;
 #endif // USE_LOGGING
-                }
-//                cv::rectangle(frame, roi, cv::Scalar(0, 255, 0));
-                drawTargetRect(frame, roi, cv::Scalar(0, 0, 255));
-                cv::imshow("Output", frame);
-            }
-            else
-            {
-                break;
-            }
-        }
-        key = cv::waitKey(20);
-    }
-    capture.release();
-    return 0;
+					}
+					if ((roi.width > 0) && (roi.height > 0))
+					{
+						drawTargetRect(frame, roi, cv::Scalar(0, 0, 255));
+					}
+					cv::imshow("Output", frame);
+				}
+				else
+				{
+					break;
+				}
+			}
+			key = cv::waitKey(20);
+		}
+		capture.release();
+	}
+	return 0;
 }
